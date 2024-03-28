ec7f42cd55354931005c5ef9b05c796fecb4ddfe
ArataUchida <158299930+ArataUchida@users.noreply.github.com>
2024-03-28 22:34:16 +0900

    Revert "6章 : カード機能"

--- kanban/forms.py
+++ kanban/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from .models import List, Card 
+from .models import List
 
 
 class UserForm(forms.ModelForm):
@@ -14,9 +14,3 @@ class ListForm(forms.ModelForm):
     class Meta:
         model = List
         fields = ("title",)
-
-class CardForm(forms.ModelForm):
-
-    class Meta:
-        model = Card
-        fields = ("title", "description", "list")
